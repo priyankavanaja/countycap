2e2993f6bb52da158e267271db7a7aa38cae4d1b
Priyanka <35795586+priyankavanaja@users.noreply.github.com>
2020-05-19 13:03:39 +0530

    Update country1.py

--- country1.py
+++ country1.py
@@ -1,12 +1,25 @@
-# Import cars data
 import pandas as pd
-cars = pd.read_csv('cars.csv', index_col = 0)
 
-# Print out country column as Pandas Series
-print(cars['country'])
+# Build cars DataFrame
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']	names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]	dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]	cpc = [809, 731, 588, 18, 200, 70, 45]
+cars_dict = { 'country':names, 'drives_right':dr, 'cars_per_cap':cpc }
+cars = pd.DataFrame(cars_dict)
+print(cars)
 
-# Print out country column as Pandas DataFrame
-print(cars[['country']])
 
-# Print out DataFrame with country and drives_right columns
-print(cars[['country','drives_right']])
+# Import pandas as pd	# Definition of row_labels
+
+row_labels = ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
+import pandas as pd	
+# Create dictionary my_dict with three key:value pairs: my_dict	
+my_dict={'country':names,'drives_right':dr,'cars_per_cap':cpc}	
+
+
+# Build a DataFrame cars from my_dict: cars	# Specify row labels of cars
+cars=pd.DataFrame(my_dict)	cars.index=row_labels
+
+
+# Print cars	# Print cars again
+print(cars)	print (cars)
